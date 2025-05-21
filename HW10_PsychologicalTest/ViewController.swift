3d93241fdfb11f756bd2db99a6f4c7593ade6198
Kate Yeh <kateyehyeh@gmail.com>
2025-05-22 07:29:49 +0800

    Initial Commit

---
+++ HW10_PsychologicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10_PsychologicalTest
+//
+//  Created by Kate Yeh on 2025/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
