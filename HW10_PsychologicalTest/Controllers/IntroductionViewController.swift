338b27c18d69a2b9b525ddf758afce9dc9d445a3
Kate Yeh <kateyehyeh@gmail.com>
2025-05-30 21:48:29 +0800

    Add all updated Swift files and project changes

---
+++ HW10_PsychologicalTest/Controllers/IntroductionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IntroductionViewController.swift
+//  HW10_PsychologicalTest
+//
+//  Created by Kate Yeh on 2025/5/27.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
